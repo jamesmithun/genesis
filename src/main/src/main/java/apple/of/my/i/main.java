4180631e8dd9082890409fba20de512213b7dfdf
jamesmithun <jamesmithun@gmail.com>
2017-07-09 16:45:10 +0530

    Update main.java

--- src/main/src/main/java/apple/of/my/i/main.java
+++ src/main/src/main/java/apple/of/my/i/main.java
@@ -2,9 +2,16 @@ package apple.of.my.i;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class main {
+public class main extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(main.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(main.class, args);
